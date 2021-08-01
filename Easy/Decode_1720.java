a2a99dfc61c33e0445b487c56ecd51d91f17170d
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-01 20:10:46 +0800

    Update Decode_1720.java

--- Easy/Decode_1720.java
+++ Easy/Decode_1720.java
@@ -2,19 +2,24 @@ package Easy;
 
 public class Decode_1720 {
     public static void main(String[] args) {
-        int arr[]={1,2,3};
+        int arr[] = {1, 2, 3};
 
-        System.out.println(1^2);
+        int decoded[]= decode(arr,1);
+        for (int i=0;i<decoded.length;i++){
+            System.out.println(decoded[i]);
+        }
+
     }
 
     public static int[] decode(int[] encoded, int first) {
         int n = encoded.length;
-        int[] res = new int[n + 1];
+        int[] decode = new int[n + 1];
 
-        for (int i = 0; i < n+1; i++) {
-            res[i]=encoded[i]^encoded[i+1];
+        decode[0] = first;
+        for (int i = 1; i < n + 1; i++) {
+            decode[i] = encoded[i - 1] ^ decode[i - 1];
         }
 
-        return res;
+        return decode;
     }
 }
