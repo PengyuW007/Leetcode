68075ec354dd9f4261621f05dd00a2c1163dece8
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-03 23:42:05 +0800

    Create RandomizedSet_$30.java

---
+++ Mid/RandomizedSet_$30.java
@@ -0,0 +1,80 @@
+package Mid;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomizedSet_$30 {
+    public static void main(String[] args) {
+        RandomizedSet   randomSet =  new RandomizedSet();
+
+        System.out.println(randomSet.insert(1));
+
+        System.out.println(randomSet.remove(2));
+
+        System.out.println(randomSet.insert(2));
+
+        System.out.println(randomSet.getRandom());
+
+        System.out.println(randomSet.remove(1));
+
+        System.out.println(randomSet.insert(2));
+
+        System.out.println(randomSet.getRandom());
+
+
+    }
+}
+
+class RandomizedSet {
+    private ArrayList<Integer> objs;
+
+
+    public ArrayList<Integer>getObjs(){
+        return objs;
+    }
+
+    /**
+     * Initialize your data structure here.
+     */
+    public RandomizedSet() {
+        objs = new ArrayList<Integer>();
+    }
+
+    /**
+     * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+     */
+    public boolean insert(int val) {
+        boolean res = false;
+        if (!objs.contains(val)) {
+            objs.add(val);
+            res = true;
+        }
+        return res;
+    }
+
+    /**
+     * Removes a value from the set. Returns true if the set contained the specified element.
+     */
+    public boolean remove(int val) {
+        boolean res = false;
+
+        if (objs.contains(val)) {
+            objs.remove(val);
+            res = true;
+        }
+        return res;
+    }
+
+    /**
+     * Get a random element from the set.
+     */
+    public int getRandom() {
+        Random random = new Random();
+
+        int len = objs.size();
+
+        int index =  random.nextInt();
+
+        return objs.get(index);
+    }
+}
