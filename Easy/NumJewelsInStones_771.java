1e29f0389475c56176ae910d55a4b6c8233f34ba
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-24 15:11:59 +0800

    Create NumJewelsInStones_771.java

---
+++ Easy/NumJewelsInStones_771.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class NumJewelsInStones_771 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        int res = 0;
+
+        for(int i = 0;i<jewels.length();i++){
+
+        }
+
+        return res;
+    }
+}
