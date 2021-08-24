3a889c80cd3bda3ea4ca07d9e3b094f9d61a18ef
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-24 16:59:41 +0800

    JewelsInStones

--- Easy/NumJewelsInStones_771.java
+++ Easy/NumJewelsInStones_771.java
@@ -2,14 +2,19 @@ package Easy;
 
 public class NumJewelsInStones_771 {
     public static void main(String[] args) {
-
+        String jewels = "aA",stones = "aAAbbbb";
+        System.out.println(numJewelsInStones(jewels,stones));
     }
 
     public static int numJewelsInStones(String jewels, String stones) {
         int res = 0;
 
         for(int i = 0;i<jewels.length();i++){
-
+            for(int j = 0;j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j)){
+                    res++;
+                }
+            }
         }
 
         return res;
