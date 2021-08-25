6f7b1217e0683bec65291d3e68f54a31e2b3e5bc
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-25 18:03:04 +0800

    Create KidsWithCandies_1431.java

---
+++ Easy/KidsWithCandies_1431.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+public class KidsWithCandies_1431 {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+            List<Boolean>kids = new ArrayList<>();
+
+
+            for(int i = 1;i<candies.length;i++){
+
+            }
+
+            return kids;
+    }
+}
