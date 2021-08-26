2587d18a4c9f2c379f8692be5962c53e0f6a43c2
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-26 16:43:27 +0800

    Create KidsWithCandies

--- Easy/KidsWithCandies_1431.java
+++ Easy/KidsWithCandies_1431.java
@@ -4,15 +4,34 @@ import java.util.ArrayList;
 import java.util.List;
 public class KidsWithCandies_1431 {
     public static void main(String[] args) {
+        int []candies = {12,1,12};
+        List<Boolean>kids = new ArrayList<>();
+        kids = kidsWithCandies(candies,10);
 
+        for(int i =0;i<kids.size();i++){
+            System.out.println(kids.get(i));
+        }
     }
 
-    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
             List<Boolean>kids = new ArrayList<>();
+            ArrayList<Integer>can = new ArrayList<>();
+            int max = candies[0];
 
+            for(int i = 0;i<candies.length;i++){
+                can.add(candies[i]);
+            }//add array to arraylist
 
-            for(int i = 1;i<candies.length;i++){
+            for(int i = 1;i<can.size();i++){
+                max = Math.max(max,candies[i]);
+            }
 
+            for(int i = 0;i<can.size();i++){
+                if(can.get(i)+extraCandies>=max){
+                    kids.add(true);
+                }else{
+                    kids.add(false);
+                }
             }
 
             return kids;
