548221084aa0dbd3b18a8b045df9cc81166d18a3
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-30 16:36:02 +0800

    Create $SumOddLengthSubarrays_1588.java

---
+++ Easy/$SumOddLengthSubarrays_1588.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class $SumOddLengthSubarrays_1588 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumOddLengthSubarrays(int[] arr) {
+       int res =0;
+
+       return res;
+    }
+}
