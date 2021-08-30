c03dad578c7f9b8d533112b62ddc270368383dfe
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-08-30 17:51:30 +0800

    Create SubtractProductAndSum_1281.java

---
+++ Easy/SubtractProductAndSum_1281.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class SubtractProductAndSum_1281 {
+    public static void main(String[] args) {
+        System.out.println(subtractProductAndSum(234));
+    }
+
+    public static int subtractProductAndSum(int n) {
+        int res = 0;
+
+        String ori = n+"";
+
+        //System.out.println(ori);
+
+        System.out.println(String.valueOf(n));
+
+        System.out.println();
+
+        return res;
+    }
+}
