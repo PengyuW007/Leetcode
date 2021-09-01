b9ea068ef807239fd686614a0871f3ddce57be68
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-01 15:52:39 +0800

    Create SumOddLengthArray_1588

---
+++ Easy/SumOddLengthSubarrays_1588.java
@@ -0,0 +1,17 @@
+package Easy;
+
+public class SumOddLengthSubarrays_1588 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int sumOddLengthSubarrays(int[] arr) {
+        int res = 0;
+        int arrLength = arr.length;
+
+        //if()
+
+
+        return res;
+    }
+}
