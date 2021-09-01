aa9986261ebd27aee3badaf9811ecfe7b92a9763
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-01 17:37:55 +0800

    Create CreateTargetArray_1389.java

---
+++ Easy/CreateTargetArray_1389.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class CreateTargetArray_1389 {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] createTargetArray(int[] nums, int[] index) {
+       int res[] = new int[index.length];
+
+       return res;
+    }
+}
