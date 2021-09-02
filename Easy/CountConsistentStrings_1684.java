54a7f34a6e840c5e48b2cbf519d998147850691b
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-02 17:17:37 +0800

    Create CountConsistentStrings_1684.java

---
+++ Easy/CountConsistentStrings_1684.java
@@ -0,0 +1,18 @@
+package Easy;
+
+public class CountConsistentStrings_1684 {
+    public static void main(String[] args) {
+        String []words = {"a","b","c","ab","ac","bc","abc"};
+
+        System.out.println(countConsistentStrings("abc",words));
+    }
+
+    public static int countConsistentStrings(String allowed, String[] words) {
+        int res = words.length;
+        for(int i = 0;i<words.length;i++){
+           
+        }
+
+        return res;
+    }
+}
