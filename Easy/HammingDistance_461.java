4324e4596cb437f463fb5c72abdb62b066ca24ec
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-19 19:03:38 +0800

    Create HammingDistance_461.java

---
+++ Easy/HammingDistance_461.java
@@ -0,0 +1,18 @@
+package Easy;
+
+public class HammingDistance_461 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int hammingDistance(int x, int y) {
+        int res = 0;
+        int xor = x^y;
+
+        while(xor!=0){
+
+        }
+
+        return res;
+    }
+}
