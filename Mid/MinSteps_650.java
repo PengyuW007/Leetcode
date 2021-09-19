89961e9dc81223c89d03a0126deca46bfe93c257
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-19 16:29:22 +0800

    Create MinSteps_650.java

---
+++ Mid/MinSteps_650.java
@@ -0,0 +1,19 @@
+package Mid;
+
+public class MinSteps_650 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minSteps(int n) {
+        int res = 0;
+
+        res=n%2;
+
+        if(res==0){
+            
+        }
+
+        return res;
+    }
+}
