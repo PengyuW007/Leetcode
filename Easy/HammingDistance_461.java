fc36933128fb216e0e3c0a2d2caed387b9aa36a7
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-20 10:57:09 +0800

    HammingDistance_461

--- Easy/HammingDistance_461.java
+++ Easy/HammingDistance_461.java
@@ -2,17 +2,19 @@ package Easy;
 
 public class HammingDistance_461 {
     public static void main(String[] args) {
-
+        System.out.println(hammingDistance(1, 4));
     }
 
     public static int hammingDistance(int x, int y) {
-        int res = 0;
-        int xor = x^y;
+        int count = 0;
+        int xor = x ^ y;//detect different bits
 
-        while(xor!=0){
-
+        //use this counter to count the number of bits are different
+        while (xor != 0) {
+            count += xor & 1;
+            xor >>= 1;
         }
 
-        return res;
+        return count;
     }
 }
