a46a33830fb0d870e95786229d63c34ff7623aed
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-21 18:33:34 +0800

    Create LengthOfLastWord_58.java

---
+++ Easy/LengthOfLastWord_58.java
@@ -0,0 +1,22 @@
+package Easy;
+
+public class LengthOfLastWord_58 {
+    public static void main(String[] args) {
+        String s = "Hello World";
+
+        System.out.println(lengthOfLastWord(s));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+}
