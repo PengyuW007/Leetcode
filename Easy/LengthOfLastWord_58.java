9bcf6953949dc29774d1f88f9c4fb0dbd15dfa15
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-22 14:51:32 +0800

    Update LengthOfLastWord_58.java

--- Easy/LengthOfLastWord_58.java
+++ Easy/LengthOfLastWord_58.java
@@ -9,11 +9,15 @@ public class LengthOfLastWord_58 {
 
     public static int lengthOfLastWord(String s) {
         int index = s.length() - 1;
-        while (s.charAt(index) == ' ') {
+        char curr = s.charAt(index);
+        while ( curr== ' ') {
+            curr = s.charAt(index);
             index--;
         }
         int wordLength = 0;
-        while (index >= 0 && s.charAt(index) != ' ') {
+
+        while ( s.charAt(index) != ' ') {
+            curr = s.charAt(index);
             wordLength++;
             index--;
 
