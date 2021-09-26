ccc6b7bbdbb00c5793d10b8b76649775f7f1adc2
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-09-26 10:10:13 +0800

    Create SquareIsWhite_1812.java

---
+++ Easy/SquareIsWhite_1812.java
@@ -0,0 +1,12 @@
+package Easy;
+
+public class SquareIsWhite_1812 {
+    public static void main(String[] args) {
+        System.out.println(squareIsWhite("a1"));
+    }
+
+    public static boolean squareIsWhite(String coordinates) {
+       return ((coordinates.charAt(0) - 'a' + 1) + (coordinates.charAt(1) - '0')) % 2 == 1;
+
+    }
+}
