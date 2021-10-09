a5fe57da7319b448065b463cb6f97fdd7835731d
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-10-09 10:41:25 +0800

    Update SortString_1370.java

--- Easy/SortString_1370.java
+++ Easy/SortString_1370.java
@@ -5,12 +5,16 @@ import java.util.Queue;
 
 public class SortString_1370 {
     public static void main(String[] args) {
-
+        System.out.println(sortString("kkk"));
     }
 
     public static String sortString(String s) {
-        ArrayList<Character>characters = new ArrayList<>();
+        //ArrayList<Character>characters = new ArrayList<>();
+        String n = "Hello";
 
+        String k = n.substring(1);
+
+        return k;
 
     }
 }
