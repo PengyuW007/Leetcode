086b6cd8a10d85743bed868d154c2564acb37bda
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2021-10-21 16:56:47 -0500

    Create MajorityElement_229.java

---
+++ Mid/MajorityElement_229.java
@@ -0,0 +1,46 @@
+package Mid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MajorityElement_229 {
+    public static void main(String[] args) {
+        List<Integer>output;
+        int nums[] = {3,2,3};
+        output = majorityElement(nums);
+
+        for (Integer integer : output) {
+          System.out.println(integer);
+        }
+    }
+
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer>output = new ArrayList<>();
+        List<Integer>temp = new ArrayList<>();
+        int fre = nums.length/3;
+        int i;
+        int count = 0;
+        for(i = 0;i< nums.length;i++){
+            temp.add(nums[i] );
+        }
+
+        temp = temp.stream().sorted().collect(Collectors.toList());
+
+        for(i = 1;i< temp.size();i++){
+            if(temp.get(i).equals(temp.get(i - 1))){
+                count++;
+            }else {
+                count++;
+                if(count>fre){
+                    System.out.println(temp.get(i-1));
+                    output.add(temp.get(i-1));
+                }
+                count = 0;
+            }
+        }
+
+        //System.out.println(output.size());
+
+        return output;
+    }
+}
