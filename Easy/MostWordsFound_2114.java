ed092535d1222530ff7095f09d23fbe2d73e0da2
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-01-19 11:29:10 -0600

    MostWordsFound_2114

---
+++ Easy/MostWordsFound_2114.java
@@ -0,0 +1,20 @@
+package Easy;
+
+public class MostWordsFound_2114 {
+    public static void main(String[] args) {
+        String[]sentences = {"please wait", "continue to fight", "continue to win"};
+        System.out.println(mostWordsFound(sentences));
+    }
+
+    public static int mostWordsFound(String[] sentences) {
+        int res = 0;
+
+        for (int i = 0; i < sentences.length; i++) {
+            String sentence = sentences[i];
+            String tokens[] = sentence.split(" ");
+            res = Math.max(res, tokens.length);
+        }
+
+        return res;
+    }
+}
---
+++ out/production/Leetcode/README.md
@@ -0,0 +1,21 @@
+# Leetcode to be a good Javaer
+
+#packages 
+Easy/Mid/Hard
+
+#Format
+FileName(QuestionName)_NoOfQuestion
+
+eg.BuildArray_1920
+
+BuildArray is the file/question's name
+
+1920 is the question's number
+
+#Note
+
+1. You can note what you think when you are doing the question
+
+2. $ before file name, means this question hasn't done yet.(eg. $xxx_1000)
+
+3. Highly recommend you to follow the decreasing rate of pass, to do practice.
