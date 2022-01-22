b495d597a4488c239919da5bae4f4c5d6acfc8e4
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-01-21 19:18:23 -0600

    Create $RemovePalindromeSub_1332.java

---
+++ Easy/$RemovePalindromeSub_1332.java
@@ -0,0 +1,15 @@
+package Easy;
+
+public class $RemovePalindromeSub_1332 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int removePalindromeSub(String s) {
+        int res = 1;
+
+
+
+        return res;
+    }
+}
