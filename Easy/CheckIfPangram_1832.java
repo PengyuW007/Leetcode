1fe52aaa1abf9cca7a28c95450426286336ff3b3
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-01-28 16:26:18 -0600

    CheckIfPangram_1832

---
+++ Easy/CheckIfPangram_1832.java
@@ -0,0 +1,19 @@
+package Easy;
+
+import java.util.HashSet;
+
+public class CheckIfPangram_1832 {
+    public static void main(String[] args) {
+        String sentence = "leetcode";
+
+        System.out.println(checkIfPangram(sentence));
+    }
+
+    public static boolean checkIfPangram(String sentence) {
+        HashSet<Character> set =new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            set.add(sentence.charAt(i));
+        }
+        return set.size()==26;
+    }
+}
