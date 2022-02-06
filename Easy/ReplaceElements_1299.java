c4ed40b2fa7afd5f8db1fdb4d68ae0102d1498ee
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-05 22:52:45 -0600

    Update ReplaceElements_1299.java

--- Easy/ReplaceElements_1299.java
+++ Easy/ReplaceElements_1299.java
@@ -2,9 +2,9 @@ package Easy;
 
 import java.util.Arrays;
 
-public class $ReplaceElements_1299 {
+public class ReplaceElements_1299 {
     public static void main(String[] args) {
-        int arr[] = {17, 18, 5, 4, 6, 1};
+        int arr[] = {400};
         int[] res = replaceElements(arr);
 
         System.out.println(Arrays.toString(res));
@@ -16,16 +16,16 @@ public class $ReplaceElements_1299 {
         int[] res = new int[len];
 
         for (int i = 0; i < len; i++) {
-            int curr = arr[i];
-            int max = arr[i];
+            int max = 0;
             for (int j = i + 1; j < len; j++) {
                 max = Math.max(max, arr[j]);
             }
-            if (curr != max) {
+            if (i == len - 1) {
+                res[i] = -1;
+            } else {
                 res[i] = max;
             }
         }
-
         return res;
     }
 }
