7201b2a2f94bd9b9d8a511cad96131cc791dc0cd
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-07 21:01:11 -0600

    Create $HammingWeight_191.java

---
+++ Easy/$HammingWeight_191.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class $HammingWeight_191 {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(00000000000000000000000000001011));
+    }
+
+    public static int hammingWeight(int n) {
+        int res = 0;
+
+        
+        return res;
+    }
+}
