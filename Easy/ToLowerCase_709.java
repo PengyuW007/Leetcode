e59ee298406d8a67351a28dc1ba06cc72c33775f
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-12 19:20:50 -0600

    Create ToLowerCase_709.java

---
+++ Easy/ToLowerCase_709.java
@@ -0,0 +1,11 @@
+package Easy;
+
+public class ToLowerCase_709 {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
