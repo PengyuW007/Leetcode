56419a90ee8d8e68b3eaa61883378549502972df
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-14 15:33:23 -0600

    Create $LuckyNumbers_1380.java

---
+++ Easy/$LuckyNumbers_1380.java
@@ -0,0 +1,36 @@
+package Easy;
+
+import java.util.*;
+import java.util.List;
+
+public class $LuckyNumbers_1380 {
+    public static void main(String[] args) {
+        int[][] matrix = {{7, 8}, {1, 2}};
+        List<Integer> res = luckyNumbers(matrix);
+
+        System.out.println(res.get(0));
+
+    }
+
+    public static List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer>res = new ArrayList<>();
+        int max = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            int min = 0;
+            int col = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                min = Math.min(min,matrix[i][j]);
+            }
+
+            for(int j = 0; j < matrix[i].length; j++){
+                if(matrix[i][j]==min){
+                    col = j;
+                }
+            }
+            max = Math.max(matrix[i][col],max);
+        }
+
+        res.add(max);
+        return res;
+    }
+}
