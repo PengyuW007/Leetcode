ec03300dcd4f65432509393cf0704a418e256f39
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-14 11:57:04 -0600

    Create $NumWays_LCP7.java

---
+++ Easy/$NumWays_LCP7.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class $NumWays_LCP7 {
+    public static void main(String[] args) {
+
+    }
+
+    public int numWays(int n, int[][] relation, int k) {
+        int res = 0;
+
+        return res;
+    }
+}
