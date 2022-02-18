70e32e37dc49b3166bb9068854b13dfbcfbb0dc9
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-17 22:51:59 -0600

    Create $MinimumSum_2160.java

---
+++ Easy/$MinimumSum_2160.java
@@ -0,0 +1,14 @@
+package Easy;
+
+public class $MinimumSum_2160 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minimumSum(int num) {
+        String numStr=num+"";
+        int res = 0;
+        return res;
+
+    }
+}
