4c7bae9ba2d0f83f15ee76d790f8e3a9d076851c
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-17 19:39:10 -0600

    Update FindCenter_1791.java

--- Easy/FindCenter_1791.java
+++ Easy/FindCenter_1791.java
@@ -1,13 +1,31 @@
 package Easy;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class FindCenter_1791 {
     public static void main(String[] args) {
         int edges[][]={{1,2},{5,1},{1,3},{1,4}};
 
-        System.out.println(findCenter(edges));
+        System.out.println(findCenter1(edges));
     }
 
-    public static int findCenter(int[][] edges) {
+    public static int findCenter1(int[][] edges){
+        ArrayList<Integer>items = new ArrayList<>();
+        int res = 0;
+
+        for(int i = 0;i< edges.length;i++){
+            for(int j = 0;j<edges[i].length;j++){
+                items.add(edges[i][j]);
+            }
+        }
+
+        items.sort(Comparator.naturalOrder());
+
+        return res;
+    }
+
+    public static int findCenter2(int[][] edges) {
         int center = 0;
 
         int row = edges[0][0];
