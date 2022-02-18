84646be5a787ca80af7cd4d30ec42efb2b790005
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-17 22:39:05 -0600

    Update $SortSentence_1859.java

--- Easy/$SortSentence_1859.java
+++ Easy/$SortSentence_1859.java
@@ -2,7 +2,7 @@ package Easy;
 
 public class $SortSentence_1859 {
     public static void main(String[] args) {
-        String s = "is2 sentence4 This1 a3";
+        String s = "Myself2 Me1 I4 and3";
 
         System.out.println(sortSentence(s));
     }
@@ -13,12 +13,14 @@ public class $SortSentence_1859 {
 
         for (int i = 0; i < inTokens.length; i++) {
             int index = inTokens[i].charAt(inTokens[i].length() - 1) - 48-1;
-            tokens[index]=inTokens[i].substring(0,inTokens.length-2);
+            tokens[index]=inTokens[i].substring(0,inTokens[i].length()-1);
         }
         String res = "";
-        for(int i = 0;i< tokens.length;i++){
-            res+=tokens[i]+ " ";
+
+        for(int i = 0;i< tokens.length-1;i++){
+            res+=tokens[i]+" ";
         }
+        res+=tokens[tokens.length-1];
 
         return res;
     }
