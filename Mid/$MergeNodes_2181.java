48a0f22bf9a4ac14b29d881f743b757f63f12d66
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-22 20:04:27 -0600

    Create $MergeNodes_2181.java

---
+++ Mid/$MergeNodes_2181.java
@@ -0,0 +1,29 @@
+package Mid;
+
+public class MergeNodes_2181 {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode mergeNodes(ListNode head) {
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
