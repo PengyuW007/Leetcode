d875c6716f8b9a1dead4102c3898f04afe85f34a
Skadoosh777 <64492688+Skadoosh777@users.noreply.github.com>
2022-02-25 12:05:12 -0600

    Update Maximum69Number_1323.java

--- Easy/Maximum69Number_1323.java
+++ Easy/Maximum69Number_1323.java
@@ -2,7 +2,7 @@ package Easy;
 
 public class Maximum69Number_1323 {
     public static void main(String[] args) {
-        System.out.println(maximum69Number(9999));
+        System.out.println(maximum69Number(9669));
     }
 
     public static int maximum69Number (int num) {
@@ -10,6 +10,7 @@ public class Maximum69Number_1323 {
         for(int i =0;i<numStr.length();i++){
             if(numStr.charAt(i)=='6'){
                 numStr=numStr.substring(0,i)+'9'+numStr.substring(i+1);
+                //numStr =numStr.replace('6','9');
                 break;
             }
         }
