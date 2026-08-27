6f2de0423f2cf5c66495b68fe5956dec5f0f7ce6
Pengyu Wang <pengyuwang777@gmail.com>
2026-08-26 22:37:17 -0400

    Create ReturnLengthOfArgumentsPassed_2703.java

---
+++ Easy/ReturnLengthOfArgumentsPassed_2703.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class ReturnLengthOfArgumentsPassed_2703 {
+    public static void main(String[] args) {
+        System.out.println(lengthOfArgs(args));
+    }
+    public static int lengthOfArgs(String[]args) {
+        return args.length;
+    }
+}
