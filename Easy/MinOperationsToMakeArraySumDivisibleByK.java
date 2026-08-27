5fab3ed486bfa6d745d727aef42c2f037818f284
Pengyu Wang <pengyuwang777@gmail.com>
2026-08-26 22:41:36 -0400

    Create MinOperationsToMakeArraySumDivisibleByK.java

---
+++ Easy/MinOperationsToMakeArraySumDivisibleByK.java
@@ -0,0 +1,10 @@
+package Easy;
+
+public class MinOperationsToMakeArraySumDivisibleByK {
+    public static void main(String[] args) {
+
+    }
+    public static int minOperations(int[] nums, int k) {
+        return 0;
+    }
+}
