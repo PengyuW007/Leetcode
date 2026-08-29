0f630317225626474f66a4ee569dbf61c74c3f0a
Pengyu Wang <pengyuwang777@gmail.com>
2026-08-29 13:30:27 -0400

    fix - ContainerWithMostWater_11

--- Mid/ContainerWithMostWater_11.java
+++ Mid/ContainerWithMostWater_11.java
@@ -3,10 +3,28 @@ package Mid;
 public class ContainerWithMostWater_11 {
     public static void main(String[] args) {
         int[] nums = {1,8,6,2,5,4,8,3,7};
-        System.out.println(maxArea(nums));
+        System.out.println(twoPointerMaxArea(nums));
+
     }
 
-    public static int maxArea(int[] height) {
+    public static int twoPointerMaxArea(int[] heights) {
+        int max = 0;
+        int left = 0,right = heights.length-1;
+
+        while(left<right) {
+            int product = (right - left) * Math.min(heights[left], heights[right]);
+
+            max = Math.max(max, product);
+            if(heights[left]<heights[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max;
+    }
+
+    public static int naiveMaxArea(int[] height) {
        int max = 0;
         int product = 0;
         for(int i = 0;i<height.length;i++){
