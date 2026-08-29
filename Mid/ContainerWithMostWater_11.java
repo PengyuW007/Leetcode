2cdd102b56d35eb5bb302bceb703599ff08e5130
Pengyu Wang <pwang2@laurentian.ca>
2026-08-29 12:02:11 -0400

    Update ContainerWithMostWater_11.java

--- Mid/ContainerWithMostWater_11.java
+++ Mid/ContainerWithMostWater_11.java
@@ -3,9 +3,24 @@ package Mid;
 public class ContainerWithMostWater_11 {
     public static void main(String[] args) {
         int[] nums = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(nums));
     }
 
     public static int maxArea(int[] height) {
-        return 0;
+       int max = 0;
+        int product = 0;
+        for(int i = 0;i<height.length;i++){
+            for(int j=height.length-1;j>=0;j--){
+                int shorter =0;
+                if(height[i]>height[j])
+                     shorter = height[j];
+                else
+                    shorter = height[i];
+                product = Math.abs(i-j)*shorter;
+                if(max<product)
+                    max = product;
+            }
+        }
+        return max;
     }
 }
